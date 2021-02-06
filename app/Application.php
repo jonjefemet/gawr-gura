0c6741efaaab398362c86319c7ac3291f19cd7e7
jonjefemet <jonshua_97@outlook.com>
2021-02-06 11:18:21 -0500

    autoload

---
+++ app/Application.php
@@ -0,0 +1,13 @@
+<?php
+namespace App;
+
+class Application
+{
+    
+    public function run()
+    {
+        print_r('panas');
+    }
+    
+}
+
--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-
 <?php
-phpinfo();
-?>
+
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+require __DIR__ . '/vendor/autoload.php';
+
+use App\Application;
+
+$application = new Application();
+$application->run();
